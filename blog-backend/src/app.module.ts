0338db59fe5843fa4ec1e59c348151a46176d6cc
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2020-02-04 02:55:10 +0100

    security improvement

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 
-const uri = "mongodb+srv://mahmut:dyrbkr21@blog-app-nsbk6.azure.mongodb.net/test?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.name}:${process.env.password}@blog-app-nsbk6.azure.mongodb.net/test?retryWrites=true&w=majority`;
 
 @Module({
   imports: [
