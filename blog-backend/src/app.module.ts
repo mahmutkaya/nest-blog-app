09b0d9df4222a0195839bb6da3b3fdb6abc68db8
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2020-02-04 02:43:55 +0100

    mongodb cloud account added to save and retrive data instead of local,
    backend port / server_base_url changed to resolve port conflict,

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import { BlogModule } from './blog/blog.module';
 
+const uri = "mongodb+srv://mahmut:dyrbkr21@blog-app-nsbk6.azure.mongodb.net/test?retryWrites=true&w=majority";
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-blog-project', {useNewUrlParser: true}),
+    MongooseModule.forRoot(uri, {useNewUrlParser: true}),
     BlogModule
   ],
   controllers: [AppController],
