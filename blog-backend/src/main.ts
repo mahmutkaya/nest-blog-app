c12f2002c51317470b6122faa463506b2ac9cb13
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2020-03-04 02:23:55 +0100

    image value added, db_url moved to .env

--- blog-backend/src/main.ts
+++ blog-backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5200);
+  await app.listen(process.env.PORT || 5200);
 }
 bootstrap();
